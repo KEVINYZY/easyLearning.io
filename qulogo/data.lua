d79c477af03bdbe931e2adcb2201c226a0336df1
Zhou Chang <achang.zhou@gmail.com>
2016-09-17 16:47:19 +0800

    Just backup
    Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

---
+++ qulogo/data.lua
@@ -0,0 +1,52 @@
+require('torch')
+require('image')
+
+local pageIndex = 1
+local patchIndex = 1
+
+local initedRandom = false
+local pageSeq = nil
+local randomBatch = function(opt, config)
+    if ( initedRandom == false) then
+        initedRandom = true
+        pageSeq = torch.randperm(config.totalNumber)
+        pageIndex = 1
+    end
+    
+    local pageNumber = pageSeq[pageIndex]
+    local fileName = opt.d .. "/" .. pageNumber .. ".jpg"
+    local fullImage = image.loadJPG(fileName)
+   
+    local batch = torch.Tensor(opt.batch_size, 3, config.inputHeight, config.inputWidth)
+    local i = 1
+    for i = 1, opt.batch_size do
+        while true do
+            local x = torch.random(config.imageWidth - config.inputWidth)
+            local y = torch.random(config.imageHeight - config.inputHeight)
+            
+            local xx = math.min(x, config.logoX)
+            local yy = math.min(y, config.logoY)
+            local xx_ = math.max(x+config.inputWidth, config.logoX + config.logoWidth)
+            local yy_ = math.max(x+config.inputHeight, config.logoY + config.logoHeight)
+            if (    (xx_ - xx) > (config.logoWidth + config.inputWidth)
+                or  (yy_ - yy) > (config.logoHeight + config.inputHeight) ) then
+                batch[{i,{},{},{}}]:copy( fullImage[{{},{yy, yy + config.inputHeight - 1},{xx, xx + config.inputWidth -1}}] )   
+                break;
+            end
+        end
+    end
+
+    batch = batch * 2 - 1;
+    local maskPos = {{},{},
+                       {config.maskTop, config.maskTop + config.maskHeight - 1},
+                       {config.maskLeft, config.maskLeft + config.maskWidth - 1} }
+
+    local centerBatch = batch[maskPos]:clone()
+    batch[maskPos]:zero()
+
+    return batch, centerBatch
+end
+
+d = {}
+d.randomBatch = randomBatch
+return d
--- qulogo/train.lua
+++ qulogo/train.lua
@@ -1,8 +1,8 @@
 require('torch')
 require('image')
-local u = require('./util')
-local d = require('./data')
-local m = require('./model')
+local util = require('./util')
+local data = require('./data')
+local model = require('./model')
 
 -- Checking input paramters and load config
 local cmd = torch.CmdLine()
@@ -12,9 +12,9 @@ cmd:option('-neck', 384, 'The middle hidden vector')
 cmd:option('-total_iterator', 100000, "Total iterate number")
 cmd:option('-batch_size', 16, "Batch number")
 cmd:option('-seed', 1979, "Random seed")
+
 local opt = cmd:parse(arg)
-assert(opt.d ~= '')
-local config = u.loadConfig(opt)
+local config = util.loadConfig(opt)
 torch.manualSeed(opt.seed)
 torch.setnumthreads(1)
 torch.setdefaulttensortype('torch.FloatTensor')
@@ -22,13 +22,43 @@ torch.setdefaulttensortype('torch.FloatTensor')
 -- Checking model input and output size
 --[[
 local x = torch.rand(5, 3, config.inputWidth, config.inputHeight)
-local gen = m.buildGenerator(opt, config)
-local enc = m.buildEncoder(opt, config)
-local disc = m.buildDiscriminator(opt, config)
+local gen = model.buildGenerator(opt, config)
+local enc = model.buildEncoder(opt, config)
+local disc = model.buildDiscriminator(opt, config)
 local z = enc:forward(x)
 local y = gen:forward(z)
 local v = disc:forward(y)
 print(z:size(), y:size(), v:size())
 --]]
 
+-- Global info
+local inputBatch, centerBatch = nil, nil
+local netG = nn.Sequential()
+local netD = model.buildDiscriminator(opt, config)
+netG:add( model.buildEncoder(opt, config) )
+netG:add( model.buildGenerator(opt, config) )
+netG:training()
+netD:training()
+
+local parametersD, gradParametersD = netD:getParameters()
+local parametersG, gradParametersG = netG:getParameters()
+
+-- Training discriminator
+local fDx = function(x) 
+    gradParametersD:zero()
+end
+
+-- Traning genrator
+local fGx = function(x)
+    gradParametersG:zero()
+end
+
+local doTrain = function()
+    for i = 1, opt.total_iterator do
+        inputBatch, centerBatch = data.randomBatch()
+         
+                 
+        collectgarbage() 
+    end
+end
 
