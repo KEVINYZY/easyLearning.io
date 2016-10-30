64791edc9a33bb6018ba67b10adb8582f0dcb48f
Zhou Chang <achang.zhou@gmail.com>
2016-10-30 13:28:19 +0800

    Just bacup
    Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

--- snd/model.lua
+++ snd/model.lua
@@ -18,6 +18,11 @@ featureCNN:add( nn.SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1) )
 featureCNN:add( nn.ReLU(true) )
 featureCNN:add( nn.SpatialMaxPooling(2, 2, 2, 2, 0, 0):ceil() )
 
+-- output is 7x7
+
+featureCNN:add(nn.SpatialConvolution(512, 1024, 1, 1, 1, 1, 0, 0))
+featureCNN:add(nn.LeakyReLU(0.1))
+
 local mbox = nn.ConcatTable()
 local allBoxes = { {2,2}, {3,3}, {4,4}, {5,5}, {6,6}, {7,7}, 
                    {2,4}, {4,2}, 
@@ -25,27 +30,21 @@ local allBoxes = { {2,2}, {3,3}, {4,4}, {5,5}, {6,6}, {7,7},
 
 for i = 1, #allBoxes do
     local boxConf = nn.Sequential()
-    boxConf:add(nn.SpatialConvolution(512, 256, 1, 1, 1, 1, 0, 0))
-    boxConf:add(nn.ReLU(true))
-    boxConf:add(nn.SpatialConvolution(256, 21, allBoxes[i][1], allBoxes[i][2], 1, 1, 0, 0))
+    boxConf:add(nn.SpatialConvolution(1024, 21, allBoxes[i][1], allBoxes[i][2], 1, 1, 0, 0))
     boxConf:add(nn.SpatialLogSoftMax())
     mbox:add(boxConf)
  
     local boxLoc = nn.Sequential()
-    boxLoc:add(nn.SpatialConvolution(512, 256, 1, 1, 1, 1, 0, 0))
-    boxLoc:add(nn.LeakyReLU(0.1))
-    boxLoc:add(nn.SpatialConvolution(256,  4, allBoxes[i][1], allBoxes[i][2], 1, 1, 0, 0))
+    boxLoc:add(nn.SpatialConvolution(1024,  4, allBoxes[i][1], allBoxes[i][2], 1, 1, 0, 0))
     mbox:add(boxLoc)
 end
 
 featureCNN:add(mbox)
 
---[[
 fixedCNN:cuda()
 featureCNN:cuda()
 local x = torch.rand(4, 3,256,256):cuda()
 local y = featureCNN:forward( fixedCNN:forward(x) )
 print(y)
---]]
 
 return {allBoxes, fixedCNN, featureCNN}
